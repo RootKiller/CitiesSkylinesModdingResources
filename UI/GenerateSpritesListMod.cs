1c7e4db77fbc68677d0df8fe7f846dc18539cd34
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2015-03-17 01:39:11 +0100

    Added sprites list and simple mod to generate updated list (untested)

---
+++ UI/GenerateSpritesListMod.cs
@@ -0,0 +1,34 @@
+using System;
+using ColossalFramework.UI;
+
+namespace GenerateSpritesListMod
+{
+	public class GenerateSpritesListMod : IUserMod
+    {
+        public string Name { 
+            get { return "GenerateSpritesListMod";  }
+        }
+
+        public string Description {
+            get { return "Lorem ipsum - non sense description"; }
+        }
+    }
+
+    public class ModLoad : LoadingExtensionBase
+    {
+    	public override void OnLevelLoaded(LoadMode mode) 
+        {
+        	using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"sprites.txt", false))
+            {
+            	UIView view = GameObject.FindObjectOfType<UIView>() as UIView;
+            	
+                // Print all sprites
+                foreach (UITextureAtlas.SpriteInfo spriteInfo in view.defaultAtlas.sprites)
+                {
+                    //DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, spriteInfo.name);
+                    file.WriteLine(spriteInfo.name);
+                }
+            }
+        }
+    }
+}
